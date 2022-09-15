9973cc416dd9c32672b34c82fef89372f39d0fa1
jorge5123 <82788317+jorge5123@users.noreply.github.com>
2022-09-15 17:33:52 -0500

    J15/09_17:53

--- shooter.py
+++ shooter.py
@@ -1,3 +1,5 @@
+from cProfile import run
+from re import S
 import pygame, random
 
 WIDTH = 800
@@ -6,6 +8,7 @@ BLACK = (0,0,0)
 WHITE = (255,255,255)
 
 pygame.init()
+pygame.mixer.init(  )
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Shooter")
 clock = pygame.time.Clock()
@@ -20,22 +23,107 @@ class Player(pygame.sprite.Sprite):
         self.rect.bottom = HEIGHT - 10
         self.speed_x = 0
     def update(self):
-        pass
+        self.speed_x = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speed_x = -5
+        if keystate[pygame.K_RIGHT]:
+            self.speed_x =  5
+        
+        self.rect.x += self.speed_x
+        
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+            
+        if self.rect.left < 0:
+            self.rect.left = 0
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx , self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+        
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("assets/meteorGrey_med1.png").convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1,10)
+        self.speedx = random.randrange(-5,5)
+
+    def update(self):
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 25:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1,10)
+            self.speedx = random.randrange(-5,5)
+    
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.image.load("assets/laser1.png").convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.y = y
+        self.speedy = -10
+
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
+              
+            
+background = pygame.image.load("assets/background.png").convert()
+
 
 all_sprites = pygame.sprite.Group()
+meteor_list = pygame.sprite.Group()
+bullets = pygame.sprite.Group() 
 
 player = Player()
 all_sprites.add(player)
+for i in range(8):
+    meteor = Meteor()
+    all_sprites.add(meteor)
+    meteor_list.add(meteor)
+
 
 running = True
+if running == False:
+    print("Perdiste")
+
 while running:
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()  
+    
     all_sprites.update()
     
-    screen.fill(BLACK)
+    hits = pygame.sprite.groupcollide(meteor_list, bullets, True , True)
+    for hit in hits:
+        meteor = Meteor()
+        all_sprites.add(meteor)
+        meteor_list.add(meteor)
+    
+    
+    
+    hits = pygame.sprite.spritecollide(player, meteor_list, True)
+    if hits:
+        running = False
+    screen.blit(background, [0,0])
+    
+    all_sprites.draw(screen)
     
     pygame.display.flip()
 pygame.quit()
